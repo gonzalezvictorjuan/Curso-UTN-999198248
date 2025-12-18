5946c1fc18e4fb55eca4f543c9c19bb61967096f
Victor Juan González <g.victor.juan@gmail.com>
2025-12-18 20:35:10 -0300

    dado en clase

--- backend-node-ts/src/index.ts
+++ backend-node-ts/src/index.ts
@@ -1,14 +1,36 @@
 import express, { Request, Response } from 'express';
+import path from 'path';
 
 const app = express();
 const PORT = 3000;
 
-// Mi Primer Endpoint
-app.get('/', (req: Request, res: Response) => {
+// Middleware para servir archivos estáticos desde la carpeta "public"
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+// Todos los endpoints van a ir desde /api/
+
+/**
+ * Endpoint raíz que responde con un mensaje JSON
+ *
+ * GET /api
+ *
+ * req: Request - Objeto de solicitud de Express
+ * res: Response - Objeto de respuesta de Express
+ *
+ * Respuesta:
+ * {
+ *   "message": "¡Hola, mundo! Este es un servidor Express con TypeScript. 🚀"
+ * }
+ */
+app.get('/api', (req: Request, res: Response) => {
   console.log('¡Alguien accedió al endpoint raíz! 🌐');
   res.json({
     message: '¡Hola, mundo! Este es un servidor Express con TypeScript. 🚀',
   });
+});
+
+app.get('/api/saludo', (req: Request, res: Response) => {
+  res.json({ mensaje: 'Hola desde la API 🚀' });
 });
 
 // Iniciar el servidor HTTP
